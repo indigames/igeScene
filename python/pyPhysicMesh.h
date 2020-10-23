c0ca091c2056f7cd7f71c3cd0cdb9f77adb05934
thai.phi <thai.phi@indigames.net>
2020-10-23 09:52:44 +0700

    Add Python binding

---
+++ python/pyPhysicMesh.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Python.h>
+
+#include "components/Component.h"
+#include "components/physic/PhysicMesh.h"
+
+#include "python/pyComponent.h"
+
+namespace ige::scene
+{
+    struct PyObject_PhysicMesh
+    {
+        PyObject_Component super;
+        PhysicMesh *component;
+    };
+
+    // Type declaration
+    extern PyTypeObject PyTypeObject_PhysicMesh;
+
+    // Dealloc
+    void PhysicMesh_dealloc(PyObject_PhysicMesh *self);
+
+    // String represent
+    PyObject *PhysicMesh_str(PyObject_PhysicMesh *self);
+
+    //! Convex
+    PyObject *PhysicMesh_isConvex(PyObject_PhysicMesh *self);
+    int PhysicMesh_setConvex(PyObject_PhysicMesh *self, PyObject *value);
+
+    //! Model path
+    PyObject *PhysicMesh_getPath(PyObject_PhysicMesh *self);
+    int PhysicMesh_setPath(PyObject_PhysicMesh *self, PyObject *value);
+} // namespace ige::scene
