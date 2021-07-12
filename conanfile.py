5d927f998dc6c5d59be04356d438cd7f51dfa8fa
thai.phi <thai.phi@indigames.net>
2021-07-12 17:23:33 +0700

    Update: use Python 3.9.1

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class IgeConan(ConanFile):
     revision_mode="scm"
 
     def requirements(self):
-        self.requires("Python/3.7.6@ige/test")
+        self.requires("Python/3.9.1@ige/test")
         self.requires("pyxCore/0.4.91@ige/test")
         self.requires("igeCore/0.4.91@ige/test")
         self.requires("json/3.9.1@ige/test")
