bbf5a0927aa21f87c489204f11b5e036e1a54588
thai.phi <thai.phi@indigames.net>
2021-12-17 09:45:44 +0700

    Revert "[Minor] quick fix transform"
    This reverts commit c39952dbe20cece13f00987bddefa94c197557fe.

--- components/TransformComponent.cpp
+++ components/TransformComponent.cpp
@@ -24,8 +24,6 @@ namespace ige::scene
         m_worldRotation = rot;
         m_worldScale = scale;
 
-        vmath_quatToEuler(m_localRotation.P(), m_originEuler.P());
-
         if (getOwner()->getParent())
         {
             m_parent = getOwner()->getParent()->getTransform();
@@ -251,12 +249,6 @@ namespace ige::scene
         return m_worldRotation * Vec3(0.f, 0.f, 1.f);
     }
 
-    void TransformComponent::setOriginEuler(const Vec3& rot)
-    {
-        m_originEuler = rot;
-        setLocalRotation(rot);
-    }
-
     void TransformComponent::updateLocalToWorld()
     {
         // Update local matrix
@@ -480,7 +472,6 @@ namespace ige::scene
         j["wpos"] = m_worldPosition;
         j["wrot"] = wRotEuler;
         j["wscale"] = m_worldScale;
-        j["originRot"] = getOriginEuler();
     }
 
     //! Deserialize
@@ -519,10 +510,6 @@ namespace ige::scene
         else if (key.compare("wscale") == 0)
         {
             setScale(val);
-        }
-        else if (key.compare("originRot") == 0) 
-        {
-            setOriginEuler(val);
         }
         else
         {
