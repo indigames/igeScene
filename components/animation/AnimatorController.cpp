d9f0c8027a59781c939828fa180379ee18139667
thai.phi <thai.phi@indigames.net>
2021-12-20 10:32:05 +0700

    WIP

--- components/animation/AnimatorController.cpp
+++ components/animation/AnimatorController.cpp
@@ -6,12 +6,38 @@ namespace ige::scene
     //! Constructor
     AnimatorController::AnimatorController()
     {
+        m_figure = nullptr;
     }
 
     // Destructor
     AnimatorController::~AnimatorController()
     {
+        m_figure = nullptr;
     }
+
+
+    void AnimatorController::setPath(const std::string& path)
+    {
+        if(m_path.compare(path) != 0) {
+            //TODO: load state machines
+
+            m_path = path;
+        }
+    }
+
+    void AnimatorController::setFigure(BaseFigure* figure)
+    {
+        if(m_figure != figure) {
+            m_figure = figure;
+        }
+    }
+
+    //! Update
+    void AnimatorController::update(float dt, bool ignoreTimeScale )
+    {
+        if(!m_figure) return;
+    }
+
 
     //! Serialize component
     void to_json(json &j, const AnimatorController &obj)
